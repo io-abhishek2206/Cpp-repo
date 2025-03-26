5b8799d1494e714e855a0d2d5163eb0df8fdfa24
Abhishek Jain <abhishekjain00e@gmail.com>
2025-03-26 17:33:53 +0530

    CODEFORCES

--- 339A-Helpful_Maths.cpp
+++ 339A-Helpful_Maths.cpp
@@ -1,55 +1,58 @@
-#include<iostream>
-#include<cstring>
+#include <iostream>
+#include <cstring>
 using namespace std;
 int main()
 {
     string s;
-    cin>>s;
-    int len=s.size();
-    int arr[3]={0};
-    int count=0;
-    for(int i=0;i<len;i++)
+    cin >> s;
+    int len = s.size();
+    int arr[3] = {0};
+    int count = 0;
+    for (int i = 0; i < len; i++)
     {
-        if(s[i]=='1') {
+        if (s[i] == '1')
+        {
             count++;
             arr[0]++;
         }
-        else if(s[i]=='2') {
+        else if (s[i] == '2')
+        {
             count++;
             arr[1]++;
         }
-        else if(s[i]=='3') {
+        else if (s[i] == '3')
+        {
             count++;
             arr[2]++;
         }
     }
-    if(count==1)
+    if (count == 1)
     {
-        cout<<s<<endl;
+        cout << s << endl;
     }
     else
     {
-        for(int i=0;i<count;i++)
+        for (int i = 0; i < count; i++)
         {
-            if(arr[0]!=0)
+            if (arr[0] != 0)
             {
-                cout<<"1";
+                cout << "1";
                 arr[0]--;
             }
-            else if(arr[1]!=0)
+            else if (arr[1] != 0)
             {
-                cout<<"2";
+                cout << "2";
                 arr[1]--;
             }
-            else if(arr[2]!=0)
+            else if (arr[2] != 0)
             {
-                cout<<"3";
+                cout << "3";
                 arr[2]--;
             }
-            if(i!=count-1)
-                cout<<"+";
+            if (i != count - 1)
+                cout << "+";
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
