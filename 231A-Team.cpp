6b5bd58fba8226a79050896ce26f1c3b7052a1ee
Abhishek Jain <abhishekjain00e@gmail.com>
2025-03-26 17:32:00 +0530

    CODEFORCES

--- 231A-Team.cpp
+++ 231A-Team.cpp
@@ -1,16 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-    int n;cin>>n;
-    int problem_count=0;
-    for(int i=0;i<n;i++)
+    int n ;
+    cin >> n ;
+    int problem_count = 0 ;
+    for ( int i = 0 ; i < n ; i++ )
     {
-        int a,b,c;
-        cin>>a>>b>>c;
-        if((a+b+c)>1)
-            problem_count++;
+        int a ,b ,c ;
+        cin >> a >> b >> c ;
+        if ( ( a + b + c ) > 1 )
+            problem_count++ ;
     }
-    cout<<problem_count<<endl;
-    return 0;
+    cout << problem_count << endl ;
+    return 0 ;
 }
