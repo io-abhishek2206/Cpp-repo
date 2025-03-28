b3143b0bdfcefcedbc3a7b8b997dffa817543aac
abhishek jain <abhishekjain00e@gmail.com>
2025-03-28 17:01:32 +0530

    “CODECHEF”

---
+++ MANYSUMS.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m;cin>>n>>m;
+        int x=m-n;
+        cout<<(m-n << 1) + 1<<endl;
+    }
+    return 0;
+}
