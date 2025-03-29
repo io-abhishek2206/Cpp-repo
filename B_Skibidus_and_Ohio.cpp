82fe6d928ca75c7ce9d23286dbac3f4234eefe04
Abhishek Jain <abhishekjain00e@gmail.com>
2025-03-29 16:43:40 +0530

    CODEFORCES (CONTEST 1003)

--- B_Skibidus_and_Ohio.cpp
+++ B_Skibidus_and_Ohio.cpp
@@ -1,31 +1,32 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-    int t;
-    cin>>t;
-    while(t--)
+    int t ;
+    cin >> t ;
+    while ( t-- )
     {
-        string s;
-        cin>>s;
-        int len=s.size();
-        if(len>1)
+        string s ;
+        cin >> s ;
+        int len = s.size() ;
+        if ( len>1 )
         {
-            int count=0;
-            for(int i=0;i<len-1 && s.size()>1;i++)
+            int count = 0 ;
+            for ( int i = 0 ; i < len - 1 && s.size() > 1 ; i++ )
             {
-                if(s[i]==s[i+1])
+                if ( s[i] == s[i+1] )
                 {
-                    count++;
-                    break;
+                    count++ ;
+                    break ;
                 }
             }
-            if(count>0)
-                cout<<1<<endl;
+            if ( count > 0 )
+                cout << 1 << endl ;
             else
-                cout<<len<<endl;
+                cout << len << endl ;
         }
         else
-            cout<<1<<endl;
+            cout << 1 << endl ;
     }
+    return 0 ;
 }
