2d20175f2877da318ed102449a204eb14ecb2929
abhishek jain <abhishekjain00e@gmail.com>
2025-03-29 16:39:07 +0530

    CODEFORCES

---
+++ B_Skibidus_and_Ohio.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        int len=s.size();
+        if(len>1)
+        {
+            int count=0;
+            for(int i=0;i<len-1 && s.size()>1;i++)
+            {
+                if(s[i]==s[i+1])
+                {
+                    count++;
+                    break;
+                }
+            }
+            if(count>0)
+                cout<<1<<endl;
+            else
+                cout<<len<<endl;
+        }
+        else
+            cout<<1<<endl;
+    }
+}
