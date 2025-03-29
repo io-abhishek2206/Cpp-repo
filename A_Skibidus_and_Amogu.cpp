01ea1763e4497a8cece2bdca1ac87d0f7583c755
Abhishek Jain <abhishekjain00e@gmail.com>
2025-03-29 16:41:01 +0530

    CODEFORCES (CONTEST 1003)

--- A_Skibidus_and_Amogu.cpp
+++ A_Skibidus_and_Amogu.cpp
@@ -1,19 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main()
+int main ()
 {
-    int t;
-    cin>>t;
-    while(t--)
+    int t ;
+    cin >> t ;
+    while ( t-- ) 
     {
-        string s;
-        cin>>s;
-        int len=s.size();
-        for(int i=0;i<len-2;i++)
-        {
-            cout<<s[i];
-        }
-        cout<<'i'<<endl;
+        string s ;
+        cin >> s ;
+        int len = s.size() ;
+        for ( int i = 0 ; i < len - 2 ; i++ )
+            cout << s[i] ;
+        cout << 'i' << endl ;
     }
-    return 0;
+    return 0 ;
 }
