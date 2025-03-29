1c7f2a5d843c486318305c9fcdc6a236a70a4b2d
abhishek jain <abhishekjain00e@gmail.com>
2025-03-29 16:36:55 +0530

    CODEFORCES

---
+++ A_Skibidus_and_Amogu.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        int len=s.size();
+        for(int i=0;i<len-2;i++)
+        {
+            cout<<s[i];
+        }
+        cout<<'i'<<endl;
+    }
+    return 0;
+}
