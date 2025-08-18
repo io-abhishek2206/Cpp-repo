bf562a488c302d3426a8c230b9ee08902584babb
Abhishek Jain <abhishekjain@Abhisheks-MacBook-Air.local>
2025-08-18 12:32:45 +0530

    LeetCode

---
+++ 74-Search_a_2D_Matrix.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        bool check = false;
+        for (int i = 0; i < m; i++) {
+            if (matrix[i][0] == target) {
+                check = true;
+                break;
+            }
+            else if (matrix[i][0] > target && i!=0) {
+                for (int j = 0; j < n; j++) {
+                    if (matrix[i - 1][j] == target) {
+                        check = true;
+                        break;
+                    }
+                }
+            }
+            if(i==m-1)
+            {
+                for (int j = 0; j < n; j++) {
+                    if (matrix[i][j] == target) {
+                        check = true;
+                        break;
+                    }
+                }
+            }
+        }
+        return check;
+    }
+};
